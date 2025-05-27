63c280189de86237cda5954f3666d4026cf07570
Delory Rodolphe <roro_spike@hotmail.com>
2025-05-27 10:58:12 +0200

    update BaseCardImgTextRowCol.vue responsive

--- components/card/BaseCardImgTextRowCol.vue
+++ components/card/BaseCardImgTextRowCol.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <div class="flex flex-col lg:flex-row justify-center items-center min-h-[50vh] p-6 gap-2 text-white text-center lg:text-left">
+  <div class="flex flex-col lg:flex-row justify-center items-center min-h-[50vh] p-6 md:px-60 gap-2 text-white text-center">
     <div class="w-full lg:w-1/2 flex justify-center items-center ">
       <Logo />
     </div>
-    <div class="w-full lg:w-2/2 px-4 flex items-center justify-center text-center lg:justify-start">
+    <div class="w-full lg:w-2/2 px-4 flex items-center justify-center text-center md:justify-start lg:text-start ">
       <p class="max-w-xl text-2xl">
         This personal library is a curated collection of useful elements and resources gathered from across the web.
         It serves as a central place to store everything that might be helpful for building future digital projects.
--- components/logo/Logo.vue
+++ components/logo/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLink to="/public" class="block max-w-[600px] w-full">
+  <NuxtLink to="/public" class="block max-w-[500px] w-full">
     <img
         class="rounded-2xl w-full h-auto object-contain"
         src="../../public/logo-sharpped.png"
